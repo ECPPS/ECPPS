5b2d68ed2a5d7fa1480e58428e3157fcce9c5594
TymianekPL <tymi@tymi.org>
2025-05-26 20:20:34 +0200

    Fix format

--- compiler/ecpps/src/main.cpp
+++ compiler/ecpps/src/main.cpp
@@ -1,10 +1,10 @@
-#include "Shared/Config.h"
 #include "Parsing/SourceMap.h"
+#include "Shared/Config.h"
 
 int main(int argc, char* argv[])
 {
      ecpps::CompilerConfig config{argc, argv};
-     ecpps::SourceMap sources{ config };
+     ecpps::SourceMap sources{config};
 
      return 0;
 }
