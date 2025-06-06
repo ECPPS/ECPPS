d3f5273d2749152077845434d875b688a78ba258
TymianekPL <tymi@tymi.org>
2025-06-06 20:53:55 +0200

    clang-format
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- compiler/platformlib/include/platformlib/identify.h
+++ compiler/platformlib/include/platformlib/identify.h
@@ -3,32 +3,30 @@
 
 namespace ecpps::platformlib
 {
-	enum struct ArchitectureBitness
-	{
-		x86_64,
-		x86_32,
-		ARM32,
-		ARM64
-	};
+     enum struct ArchitectureBitness
+     {
+          x86_64,
+          x86_32,
+          ARM32,
+          ARM64
+     };
 
-	enum struct OperatingSystem
-	{
-		Windows,
-		WindowsOnWindows,
-		Linux,
-	};
+     enum struct OperatingSystem
+     {
+          Windows,
+          WindowsOnWindows,
+          Linux,
+     };
 
-	struct PlatformIdentity
-	{
-		ArchitectureBitness	bitness;
-		explicit PlatformIdentity(const ArchitectureBitness bitness)
-			: bitness(bitness)
-		{}
+     struct PlatformIdentity
+     {
+          ArchitectureBitness bitness;
+          explicit PlatformIdentity(const ArchitectureBitness bitness) : bitness(bitness) {}
 
-		/// <summary>
-		/// Calling this from other platform than x86 is undefined
-		/// </summary>
-		/// <returns>Current platform identity (x86)</returns>
-		static PlatformIdentity Identify(void);
-	};
-}
+          /// <summary>
+          /// Calling this from other platform than x86 is undefined
+          /// </summary>
+          /// <returns>Current platform identity (x86)</returns>
+          static PlatformIdentity Identify(void);
+     };
+} // namespace ecpps::platformlib
--- compiler/platformlib/include/platformlib/platformlib.h
+++ compiler/platformlib/include/platformlib/platformlib.h
@@ -4,10 +4,8 @@
 
 namespace ecpps::platformlib
 {
-	struct NativeException : std::runtime_error
-	{
-		explicit NativeException(std::string message)
-			: runtime_error(std::move(message))
-		{}
-	};
-}
+     struct NativeException : std::runtime_error
+     {
+          explicit NativeException(std::string message) : runtime_error(std::move(message)) {}
+     };
+} // namespace ecpps::platformlib
