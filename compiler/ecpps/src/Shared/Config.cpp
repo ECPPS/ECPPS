693c803a0df5efbd636c21420b856da0847fe944
TymianekPL <tymi@tymi.org>
2025-06-08 09:03:34 +0200

    basic generic linker flags

--- compiler/ecpps/src/Shared/Config.cpp
+++ compiler/ecpps/src/Shared/Config.cpp
@@ -1,5 +1,8 @@
 #include "Config.h"
+#include <cctype>
 #include <print>
+#include <ranges>
+#include <string>
 #include <string_view>
 #include "../Machine/ABI.h"
 #include "../Machine/Machine.h"
@@ -30,10 +33,37 @@ ecpps::CompilerConfig::CompilerConfig(int argc, char* argv[])
                auto flag = fullArgument.substr(1);
                auto value = flag.substr(flag.find(':') + 1);
                flag = flag.substr(0, flag.find(':'));
+               const auto lowerFlag =
+                   flag |
+                   std::views::transform([](const char ch) -> char
+                                         { return static_cast<char>(std::tolower(static_cast<int>(ch))); }) |
+                   std::ranges::to<std::string>();
+               const auto lowerValue =
+                   value |
+                   std::views::transform([](const char ch) -> char
+                                         { return static_cast<char>(std::tolower(static_cast<int>(ch))); }) |
+                   std::ranges::to<std::string>();
 
                if (flag == "/WX") this->warningsAreErrors = true;
                else if (flag == "/-WX")
                     this->warningsAreErrors = false;
+               else if (lowerFlag == "/output" || lowerFlag == "/out")
+               {
+                    if (!this->outputImage.empty()) {} // TODO: Warning
+                    this->outputImage = value;
+               }
+               else if (lowerFlag == "/image")
+               {
+                    if (lowerValue == "win64" || lowerValue == "pe64" || lowerValue == "pe32p")
+                         this->linker = LinkerUsed::Windows64;
+                    else if (lowerValue == "win32" || lowerValue == "pe32")
+                         this->linker = LinkerUsed::Windows32;
+                    else
+                    {
+                         // TODO: Error
+                         std::println("Unknown linker: `{}`", value);
+                    }
+               }
                else
                {
                     // TODO: Error list of some sort on invalid flag
