efd8394f725bc7616c4fbf7d9c223fe7ab6fa806
TymianekPL <tymi@tymi.org>
2025-09-21 11:49:14 +0200

    clang format
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- compiler/ecpps/src/Linker/dllHelp.h
+++ compiler/ecpps/src/Linker/dllHelp.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 std::unordered_map<std::string, std::vector<std::string>> GetExportsFromDlls(const std::vector<std::string>& dlls);
