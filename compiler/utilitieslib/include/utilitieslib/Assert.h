807f8040837b829edd735d4915e26a98a32764f4
TymianekPL <tymi@tymi.org>
2025-10-19 16:18:30 +0200

    clang-format
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- compiler/utilitieslib/include/utilitieslib/Assert.h
+++ compiler/utilitieslib/include/utilitieslib/Assert.h
@@ -8,12 +8,20 @@ void RuntimeAssert(bool condition, std::string_view conditionString, std::string
 
 #ifdef NDEBUG
 #define runtime_assert(condition, message)
-template <typename T>
-struct Empty
+template <typename T> struct Empty
 {
-     explicit(false) constexpr Empty(auto o) requires std::is_constructible_v<T, decltype(o)> {}
-     void operator=(auto o) requires std::is_copy_assignable_v<T, decltype(o)> {}
-     void operator=(auto o) requires std::is_move_assignable_v<T, decltype(o)> {}    
+     explicit(false) constexpr Empty(auto o)
+          requires std::is_constructible_v<T, decltype(o)>
+     {
+     }
+     void operator=(auto o)
+          requires std::is_copy_assignable_v<T, decltype(o)>
+     {
+     }
+     void operator=(auto o)
+          requires std::is_move_assignable_v<T, decltype(o)>
+     {
+     }
 };
 template <typename T> using DebugOnlyImpl = Empty<T>;
 #ifdef _MSC_VER
