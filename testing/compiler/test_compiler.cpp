4cf3509bf868491cb7edf659f0cdc59927315c99
Tymi <tymi@tymi.org>
2025-12-28 10:32:52 +0100

    Update test_compiler.cpp

--- testing/compiler/test_compiler.cpp
+++ testing/compiler/test_compiler.cpp
@@ -1,35 +1,7 @@
-// NOLINTBEGIN
+#include <print>
 
-#ifdef __ecpps_version
-[[dllimport]] extern "C" void ExitProcess(int);
-[[dllimport]] extern "C" void DebugBreak();
-#else
-extern "C" void ExitProcess(int);
-extern "C" void DebugBreak();
-#endif
-
-short One()
+int main(void)
 {
-     char x = 97;
-     return x;
+     std::println("Test");
+     return 0;
 }
-
-int Two()
-{
-     char x = 97;
-     return x;
-}
-
-unsigned int Three()
-{
-     unsigned char x = 97;
-     return x;
-}
-
-int main() { return One() + Two() + Three(); }
-
-#ifdef __ecpps_version
-extern "C" void _EntryPoint() { ExitProcess(main()); }
-#endif
-
-// NOLINTEND
