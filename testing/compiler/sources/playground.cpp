6807f4fd45fe540ce03a7ee2728b7e095730c641
TymianekPL <tymi@tymi.org>
2026-02-28 14:00:02 +0100

    playground

--- testing/compiler/sources/playground.cpp
+++ testing/compiler/sources/playground.cpp
@@ -9,7 +9,11 @@
 DLLIMPORT extern "C" void ExitProcess(unsigned);
 DLLIMPORT extern "C" void DebugBreak();
 
-int main() { decltype(auto) x = 0 [auto("0")]; }
+int main()
+{
+     const char* meow1 = "0";
+     return (meow1 - meow1)["a"];
+}
 
 #ifdef __ecpps_version
 extern "C" void _EntryPoint() { ExitProcess(main()); }
