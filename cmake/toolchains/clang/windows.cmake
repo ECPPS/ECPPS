37fdd6f186576867d55a537d76e1399613ace48f
TymianekPL <tymi@tymi.org>
2026-04-12 18:03:21 +0200

    clang quotes
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- cmake/toolchains/clang/windows.cmake
+++ cmake/toolchains/clang/windows.cmake
@@ -3,4 +3,4 @@ include(${CMAKE_CURRENT_LIST_DIR}/../base.cmake)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(CMAKE_EXE_LINKER_FLAGS -Xlinker --stack:10000000)
+set(CMAKE_EXE_LINKER_FLAGS "-Xlinker --stack:10000000")
