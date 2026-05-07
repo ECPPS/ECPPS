befea96ac2ade48aa64b70bf6b442d2f5d7a024f
Tymi <tymi@tymi.org>
2026-05-07 18:19:41 +0200

    Optimise the x86 code emitter (#198)
    * Initial opcodes using the new scheme
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * Complete MOV mechanics (no mov extend) for the x86-64 emitter using the new interface
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * Streamline movsx/movsxd to use the new interface
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * use the new interface for x86-64 opcodes
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * use the new interface for x86-64 opcodes
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    * Properly convert sign integers
    
    Signed-off-by: TymianekPL <tymi@tymi.org>
    
    ---------
    
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- testing/compiler/sources/playground.cpp
+++ testing/compiler/sources/playground.cpp
@@ -1,11 +1,2 @@
 #include <cstdio>
-
-#define EXPAND(x) #x
-
-#define STRINGIFY(x) EXPAND(x)
-
-int main()
-{
-     std::puts(STRINGIFY(__FILE__));
-     puts(STRINGIFY(__LINE__));
-}
+int main() { return 0; }
