62efae962ae6e26cf18542ec6351823fd6a9d1ef
TymianekPL <tymi@tymi.org>
2026-08-30 20:02:22 +0200

    Properly register backends & comment out ARM64
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- compiler/ecpps/src/Machine/Encoders/API/Target.h
+++ compiler/ecpps/src/Machine/Encoders/API/Target.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "CodeGeneration/AbstractNodes.h"
 #include "Machine/Encoders/API/Platform.h"
+#include "Machine/Encoders/API/SDK.h"
 #include "Machine/Encoders/API/VirtualInstructionEncoder.h"
 
 namespace ecpps::abi::api
@@ -12,7 +13,7 @@ namespace ecpps::abi::api
      {
           std::unique_ptr<VirtualInstructionEncoder> encoder; // TODO: small unique pointer
           std::unique_ptr<PlatformBase> platform;             // TODO: small unique pointer
-          std::unique_ptr<VirtualInstructionEncoder> sdk;     // TODO: small unique pointer
+          std::unique_ptr<SDKBase> sdk;                       // TODO: small unique pointer
           std::vector<std::unique_ptr<VirtualInstructionEncoder>> extensions;
 
           std::unique_ptr<ir::abstract::VirtualRegisterMap> registerMap;
