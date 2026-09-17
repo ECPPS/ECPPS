b90ed7683b83a829f0a572b7bf8e4862c273c297
TymianekPL <tymi@tymi.org>
2026-09-17 20:00:26 +0200

    yeet old code away (1)
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- compiler/ecpps/src/CodeGeneration/Nodes.cpp
+++ compiler/ecpps/src/CodeGeneration/Nodes.cpp
@@ -1,11 +1,1 @@
 #include "Nodes.h"
-#include <TypeSystem/TypeBase.h>
-#include <format>
-#include <variant>
-#include "../Parsing/Tokeniser.h"
-
-std::string ecpps::codegen::Routine::GenerateName(void)
-{
-     static std::size_t index{};
-     return ".LOC" + std::to_string(++index);
-}
